14e674beeeb1d4d948d89c4c1ff867e87d3d0e72
yinrongjing <1921796491@qq.com>
2026-07-07 10:13:09 +0800

    数据库2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,9 @@ import { PrismaClient } from '../generated/prisma/client.js';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 const prisma = new PrismaClient({
-  adapter: new PrismaBetterSqlite3({ url: './dev.db' }),
+  adapter: new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL?.replace('file:', '') || './dev.db'
+  }),
 });
 
 export default prisma;
